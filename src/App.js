c95975f43d4716491f9b2304d8376c60c1cde5ec
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-07-20 14:43:55 +0400

    game features new addons

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends React.Component {
     super();
 
     this.state = {
-      count: Math.floor(Math.random() * 6073)
+      count: Math.floor(Math.random() * 6073),
+      num: Math.floor(20 * Math.random() + 1)
     };
 
     this.handleAdd = this.handleAdd.bind(this);
@@ -17,7 +18,8 @@ class App extends React.Component {
   handleAdd() {
     this.setState(prevState => {
       return {
-        count: prevState.count + 1
+        count: prevState.count + 1,
+        
       };
     });
   }
@@ -25,7 +27,7 @@ class App extends React.Component {
   handleDouble() {
     this.setState(prevState => {
       return {
-        count: prevState.count * 2
+        count: prevState.count * 2,
       };
     });
   }
@@ -37,6 +39,8 @@ class App extends React.Component {
       };
     });
   }
+
+  
 
   render() {
     return (
@@ -51,8 +55,8 @@ class App extends React.Component {
         <button className="half" onClick={this.handleHalf}>
           Half
         </button>
-          <h3 className='win-text'>{this.state.count === 1 ? "WIN !" : ""}</h3>
-        <h3 className="text">Speculate with the given number to have 1.</h3>
+          <h3 className='win-text'>{this.state.count === this.state.num ? "WIN !" : ""}</h3>
+        <h3 className="text">Speculate with the given number to have <span style={{color: 'red'}}>{this.state.num}</span>.</h3>
       </div>
     );
   }
